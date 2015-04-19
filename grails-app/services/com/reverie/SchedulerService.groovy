5531bba58a5202f17cd684e53e357d0cedf86e8b
isko-algorithm <gjpgagno@gmail.com>
2015-04-19 14:35:03 +0800

    added pseudocode for scheduler

--- grails-app/services/com/reverie/SchedulerService.groovy
+++ grails-app/services/com/reverie/SchedulerService.groovy
@@ -8,6 +8,20 @@ class SchedulerService {
     def reDraw(){
         int i=0, j=0
         int index = 0
+        /*
+            if fit  //before deadline and fits exactly
+                fit schedule
+                index = 0
+            else
+                if end of list
+                    skip to the end of next habit
+                    index = 0
+                else
+                    index++
+             */
+        Task[] tasks = Task.list()
+        Habit[] habits = Habit.list()
+
     }
 
     def fit(){
